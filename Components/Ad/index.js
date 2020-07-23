26f7be8368d8285763a71ae1d4ff45a2ef54344c
Roberto Guerra <roberto.santa@semparar.com.br>
2020-07-22 23:14:41 -0300

    fix ads

--- Components/Ad/index.js
+++ Components/Ad/index.js
@@ -4,11 +4,10 @@ const Ad = ({ slotId, width, height }) => {
   useEffect(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
-
   return (
     <ins
       className="adsbygoogle"
-      style="display:block"
+      style={{ display: block }}
       data-ad-client="ca-pub-2270636537108959"
       data-ad-format="auto"
       data-full-width-responsive="true"
