773163fba5246d38eed381df2ddbd13c97edc0ed
Roberto Guerra <roberto.santa@semparar.com.br>
2020-07-22 23:04:08 -0300

    add script ad

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,11 @@ export default function Home({ categories, groups }, props) {
       <Head>
         <title>LinkGroups - Grupos do whatsapp</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          type="text/javascript"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
       </Head>
       <Header />
       <Categories categories={categories} />
